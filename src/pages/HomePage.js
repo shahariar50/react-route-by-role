220149e7290472c31f3e05abc7c68ec5f1009d9e
Shahariar Touhid <shahariar.touhid@gmail.com>
2021-05-05 10:14:08 +0600

    added homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,21 @@
-import React from "react";
+import UserContext from "context/UserContext";
+import React, { useContext } from "react";
+import { Redirect } from "react-router";
 
 const HomePage = () => {
-  return <div>Home</div>;
+  const { user } = useContext(UserContext);
+
+  if (!user.type) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <ul>
+      <li>Both View</li>
+      {user.type === "admin" && <li>Admin View</li>}
+      {user.type === "user" && <li>User View</li>}
+    </ul>
+  );
 };
 
 export default HomePage;
